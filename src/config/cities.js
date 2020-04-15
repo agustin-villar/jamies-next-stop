a8e293780c9ec90bc002a62dba256cd19a4c7a1e
Agustin Villar <agustin.v@endouble.com>
2020-04-15 23:20:25 +0200

    add cities and carriers info

---
+++ src/config/cities.js
@@ -0,0 +1,19 @@
+const cities = [
+    {
+        name: 'Amsterdam',
+        id: 'AMS',
+        key: '249758'
+    },
+    {
+        name: 'Budapest',
+        id: 'BUD',
+        key: '187423',
+    },
+    {
+        name: 'Madrid',
+        id: 'MAD',
+        key: '308526',
+    },
+];
+
+export default cities;
