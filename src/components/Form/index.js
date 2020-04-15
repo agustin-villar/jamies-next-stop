d63b59c11a028acc8c6b7af781342d10a23ae35c
Agustin Villar <agustin.v@endouble.com>
2020-04-16 00:08:50 +0200

    Multiselect component first approach

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+
 import Input from "../Input";
+import MultiSelect from "../MultiSelect";
+import cities from "../../config/cities";
 
 const Form = () => (
     <form>
         <Input label="Flying from:" name="fly_from" onChange={(data) => console.log(data)} />
-        <div>
-            <span>Flying to:</span>
-            <button>Select one or more cities</button>
-            <label><input name="fly_to" type="checkbox" value="AMS"/>Amsterdam</label>
-            <label><input name="fly_to" type="checkbox" value="BUD"/>Budapest</label>
-            <label><input name="fly_to" type="checkbox" value="MAD"/>Madrid</label>
-        </div>
+        <MultiSelect
+            label="Flying to:"
+            name="fly_to"
+            defaultText="Select one or more cities"
+            options={cities}
+            onChange={(data) => console.log(data)}
+        />
         <Input label="Flight date:" name="date_from" type="date" onChange={(data) => console.log(data)} />
-        <button type="submit"/>
+        <button type="submit">Search</button>
     </form>
 );
 
---
+++ src/components/MultiSelect/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function getCheckedOptions(options, option, addOption) {
+    if (addOption && !options.includes(option)) {
+        return [option, ...options];
+    }
+
+    if (!addOption) {
+        return options.filter((options) => !options.includes(option));
+    }
+
+    return [...options];
+}
+
+function getCheckedOptionsLabel(options, checkedOptions) {
+    return checkedOptions.map((acronym) => options.find(({ id }) => acronym === id).name).join(', ');
+}
+
+const MultiSelect = ({ label, name, defaultText, options, onChange }) => {
+    const [checkedOptions, setCheckedOptions] = useState([]);
+    const currentChoice = checkedOptions.length > 0 ? getCheckedOptionsLabel(options, checkedOptions) : defaultText;
+
+    return (
+        <div>
+            <span>{label}</span>
+            <button>{currentChoice}</button>
+            {options.map(({ name: optionName, id }) => (
+                <label key={id}>
+                    <input
+                        type="checkbox"
+                        name={name}
+                        value={id}
+                        onChange={(e) => {
+                            const { target: { value, checked } } = e;
+                            const newCheckedOptions = getCheckedOptions(checkedOptions, value, checked);
+                            onChange(newCheckedOptions);
+                            setCheckedOptions(getCheckedOptions(newCheckedOptions));
+                        }}
+                    />
+                    {optionName}
+                </label>
+            ))}
+        </div>
+    );
+}
+
+MultiSelect.propTypes = {
+    defaultText: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    options: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired
+    })).isRequired,
+};
+
+export default MultiSelect;
