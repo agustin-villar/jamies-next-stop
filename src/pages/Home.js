2509fb0070380c9eec2166698748418af04a5a21
Agustin Villar <agustin.v@endouble.com>
2020-04-15 22:14:06 +0200

    basic bootstraping

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <header>
+                <h1>Hello Jamie!</h1>
+            </header>
+            <footer>
+                Bon voyage ! <span role="img" aria-label="wave emoji">👋</span>
+            </footer>
+        </div>
+    );
+}
+
+export default Home;
